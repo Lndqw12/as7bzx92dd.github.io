7455e765b8b972bddf44042dad85fdb061345843
as7bzx92dd <g04032016@mail.ru>
2016-04-30 17:31:28 +0300

    site v46

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-$.ajax({
-    url: "https://formspree.io/g25092011@mail.ru", 
-    method: "POST",
-    data: {message: "hello!"},
-    dataType: "json"
-});
+$(document).ready(function(){
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "POST", //Метод отправки
+            url: "https://formspree.io/g25092011@mail.ru", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отпрвлено!");
+            });
+    });
+}); 
